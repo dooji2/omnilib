d3d13a462f8bd81cbed690e2cbcc6b5649552db2
dooji2 <cmir79411@gmail.com>
2024-12-24 23:29:47 +0200

    Add helper methods and documentation

--- src/client/java/com/dooji/omnilib/ui/OmniField.java
+++ src/client/java/com/dooji/omnilib/ui/OmniField.java
@@ -3,6 +3,7 @@ package com.dooji.omnilib.ui;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -22,8 +23,9 @@ public class OmniField extends ClickableWidget {
     private boolean cursorVisible;
 
     private final int cursorColor;
-    private final int backgroundColor;
-    private final int hoveredColor;
+    private int backgroundColor;
+    private int hoveredColor;
+    private int textColor = 0xFFFFFF;
     private final Identifier cursorTexture;
     private final Identifier backgroundTexture;
     private final Identifier hoveredTexture;
@@ -74,6 +76,15 @@ public class OmniField extends ClickableWidget {
 
     public void setMaxLength(int maxLength) {
         this.maxLength = maxLength;
+    }
+
+    public void setColor(int backgroundColor, int hoveredColor) {
+        this.backgroundColor = backgroundColor;
+        this.hoveredColor = hoveredColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
     }
 
     @Override
@@ -328,6 +339,6 @@ public class OmniField extends ClickableWidget {
     }
 
     @Override
-    protected void appendClickableNarrations(net.minecraft.client.gui.screen.narration.NarrationMessageBuilder builder) {
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
     }
 }
